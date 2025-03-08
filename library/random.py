90b673f41e8d1f0a61a100390ec05c188b2a4bb2
Joseph <perryjd@cardiff.ac.uk>
2025-03-08 10:36:18 +0000

    Synced library

--- library/random.py
+++ library/random.py
@@ -1,4 +1,7 @@
-###! no imports
+import os
+import random
+import numpy as np
+import tensorflow as tf
 
 # split key n times with quadratic function
 # type: (int, int, int, bool) -> List[int]
@@ -7,3 +10,11 @@ def split_key(key, n=2, mod=10**9, verbose=False):
 	keys = [int((i*key**2) % mod) for i in range(1,1+n)]
 	if verbose: print(keys)
 	return keys
+
+# type: (int) -> None
+def seed_everything(key):
+    random.seed(key)
+    np.random.seed(key)
+    tf.random.set_seed(key)
+    os.environ['PYTHONHASHSEED'] = str(key)
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
